a3a4f7471f1c12cef2834645edb5d6b1cc633d83
MFomichev <mikhail.fomichev94@gmail.com>
2013-09-27 11:10:49 -0700

    Update and rename ShiftModel.java to AbstractModel.java

---
+++ AbstractModel.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+/**
+ * Абстрантная модель. Остальные модели рекомендуется наследовать от неё
+ */
+public abstract class AbstractModel {
+    protected byte[] value = new byte[1024];
+
+    /**
+     * Заполняет массив случайным образом
+     *
+     * @param random Генератор случайных чисел
+     */
+    public AbstractModel(Random random) {
+        for (int i = 0; i < value.length; i++) {
+            value[i] = (byte) random.nextInt(2);
+        }
+    }
+    
+    @Override
+    public String toString()
+    {
+        StringBuilder builder = new StringBuilder();
+        for(byte b:value)
+        {
+            builder.append(b);
+        }
+        return builder.toString();
+    }
+}
